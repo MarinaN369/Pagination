48e680c1e2ef37c463fd75e22a6d75602042c77b
MarinaN369 <143936204+MarinaN369@users.noreply.github.com>
2024-03-04 22:17:32 +0300

    pagination finish

--- script.js
+++ script.js
@@ -1,85 +1,85 @@
-// console.log(`jay shree ram`);
+// // console.log(`jay shree ram`);
 
-let pagination = document.querySelector('ul');
+// let pagination = document.querySelector('ul');
 
-let pageNumber = 10;
-let totalPage = 20;
+// let pageNumber = 10;
+// let totalPage = 20;
 
-pagination.innerHTML = paginationFunction(totalPage, pageNumber);
-function paginationFunction(totalPage, pageNumber) {
-    let beforePage = pageNumber - 1, afterPage = pageNumber + 1;
-    let listTag = "";
-
-
-    if (pageNumber > 1) {
-        listTag += `<li class="prev btn"  onclick = "paginationFunction(totalPage, ${pageNumber - 1})"><span><<</span></li>`;
-    }
-    if (pageNumber > 1) {
-        listTag += `<li class="prev btn"  onclick = "paginationFunction(totalPage, ${pageNumber - 1})"><span><</span></li>`;
-    }
-
-    if (pageNumber > 2) {
-        listTag += `<li class=" btn" onclick = "paginationFunction(totalPage, 1)"><span>1</span></li>`;
-
-        if (pageNumber > 3) {
-            listTag += `<li class="dots"><span>...</span></li>`;
-        }
-    }
+// pagination.innerHTML = paginationFunction(totalPage, pageNumber);
+// function paginationFunction(totalPage, pageNumber) {
+//     let beforePage = pageNumber - 1, afterPage = pageNumber + 1;
+//     let listTag = "";
 
 
+//     if (pageNumber > 1) {
+//         listTag += `<li class="prev btn"  onclick = "paginationFunction(totalPage, ${pageNumber - 1})"><span><<</span></li>`;
+//     }
+//     if (pageNumber > 1) {
+//         listTag += `<li class="prev btn"  onclick = "paginationFunction(totalPage, ${pageNumber - 1})"><span><</span></li>`;
+//     }
 
-    if (pageNumber == totalPage) {
-        beforePage = beforePage - 2;
-    } else if (pageNumber == totalPage - 1) {
-        beforePage = beforePage - 1;
-    }
+//     if (pageNumber > 2) {
+//         listTag += `<li class=" btn" onclick = "paginationFunction(totalPage, 1)"><span>1</span></li>`;
 
-    if (pageNumber == 1) {
-        afterPage = afterPage + 2;
-    } else if (pageNumber == 2) {
-        afterPage = afterPage + 1;
-    }
-
-    for (let index = beforePage; index <= afterPage; index++) {
-
-        if (totalPage < index) {
-            break;
-        }
+//         if (pageNumber > 3) {
+//             listTag += `<li class="dots"><span>...</span></li>`;
+//         }
+//     }
 
 
-        if (index == 0) {
-            index = index + 1;
-        }
 
-        if (pageNumber == index) {
-            active = "active"
-        } else {
-            active = "";
-        }
+//     if (pageNumber == totalPage) {
+//         beforePage = beforePage - 2;
+//     } else if (pageNumber == totalPage - 1) {
+//         beforePage = beforePage - 1;
+//     }
 
-        listTag += `<li class="btn ${active}" onclick = "paginationFunction(totalPage, ${index})"><span>${index}</span></li>`;
+//     if (pageNumber == 1) {
+//         afterPage = afterPage + 2;
+//     } else if (pageNumber == 2) {
+//         afterPage = afterPage + 1;
+//     }
 
-    }
+//     for (let index = beforePage; index <= afterPage; index++) {
+
+//         if (totalPage < index) {
+//             break;
+//         }
 
 
-    if (pageNumber < totalPage - 1) {
+//         if (index == 0) {
+//             index = index + 1;
+//         }
 
-        if (pageNumber < totalPage - 2) {
-            listTag += `<li class="dots"><span>...</span></li>`;
-        }
+//         if (pageNumber == index) {
+//             active = "active"
+//         } else {
+//             active = "";
+//         }
 
-        listTag += `<li class=" btn" onclick = "paginationFunction(totalPage, ${totalPage})"><span>${totalPage}</span></li>`;
-    }
+//         listTag += `<li class="btn ${active}" onclick = "paginationFunction(totalPage, ${index})"><span>${index}</span></li>`;
 
-    if (totalPage > pageNumber) {
-        listTag += `<li class="next btn" onclick = "paginationFunction(totalPage, ${pageNumber + 1})"><span>>></span></li>`;
-    }
-    if (totalPage > pageNumber) {
-        listTag += `<li class="next btn" onclick = "paginationFunction(totalPage, ${pageNumber + 1})"><span>></span></li>`;
-    }
-    pagination.innerHTML = listTag;
-    return listTag;
-}
+//     }
+
+
+//     if (pageNumber < totalPage - 1) {
+
+//         if (pageNumber < totalPage - 2) {
+//             listTag += `<li class="dots"><span>...</span></li>`;
+//         }
+
+//         listTag += `<li class=" btn" onclick = "paginationFunction(totalPage, ${totalPage})"><span>${totalPage}</span></li>`;
+//     }
+
+//     if (totalPage > pageNumber) {
+//         listTag += `<li class="next btn" onclick = "paginationFunction(totalPage, ${pageNumber + 1})"><span>>></span></li>`;
+//     }
+//     if (totalPage > pageNumber) {
+//         listTag += `<li class="next btn" onclick = "paginationFunction(totalPage, ${pageNumber + 1})"><span>></span></li>`;
+//     }
+//     pagination.innerHTML = listTag;
+//     return listTag;
+// }
 // let pagination = document.querySelector('ul');
 
 // let pageNumber = 10;
@@ -126,3 +126,52 @@ function paginationFunction(totalPage, pageNumber) {
 //     };
 //     return li;
 // }
+let pagination = document.querySelector('ul');
+
+let pageNumber = 1; // начинаем с первой страницы
+let totalPage = 20;
+
+pagination.innerHTML = paginationFunction(totalPage, pageNumber);
+
+function paginationFunction(totalPage, pageNumber) {
+    let listTag = "";
+    let startPage = pageNumber;
+    let endPage = pageNumber + 2;
+
+    listTag += `<li class="prev btn" onclick="paginationFunction(totalPage, ${pageNumber - 1})"><span><<</span></li>`;
+    listTag += `<li class="prev2 btn" onclick="paginationFunction(totalPage, ${pageNumber - 2})"><span><</span></li>`;
+
+    if (pageNumber > 2) {
+        listTag += `<li class="btn" onclick="paginationFunction(totalPage, 1)"><span>1</span></li>`;
+        if (pageNumber > 3) {
+            listTag += `<li class="dots"><span>...</span></li>`;
+        }
+    }
+
+    if (pageNumber > totalPage - 2) {
+        startPage = totalPage - 4;
+        endPage = totalPage;
+    }
+
+    for (let index = startPage; index <= endPage; index++) {
+        if (index < 1 || index > totalPage) {
+            continue;
+        }
+
+        let active = (pageNumber === index) ? "active" : "";
+        listTag += `<li class="btn ${active}" onclick="paginationFunction(totalPage, ${index})"><span>${index}</span></li>`;
+    }
+
+    if (pageNumber < totalPage - 1) {
+        if (pageNumber < totalPage - 3) {
+            listTag += `<li class="dots"><span>...</span></li>`;
+        }
+        listTag += `<li class="next btn" onclick="paginationFunction(totalPage, ${pageNumber + 1})"><span>></span></li>`;
+    }
+    if (pageNumber < totalPage - 2) {
+        listTag += `<li class="next2 btn" onclick="paginationFunction(totalPage, ${pageNumber + 2})"><span>>></span></li>`;
+    }
+
+    pagination.innerHTML = listTag;
+    return listTag;
+}
